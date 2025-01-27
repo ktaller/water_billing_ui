aa43ce6f9778b74b3cfe03dead7f86e21406ae14
George Ngigi <ngigegeorge023@gmail.com>
2025-01-27 15:24:45 +0300

    Added user registration to link with server.

--- lib/screens/user_registration.dart
+++ lib/screens/user_registration.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:water_billing_ui/constants/constants.dart';
+
 import 'form_controller.dart';
 
 class FormPage extends StatelessWidget {
@@ -17,7 +22,7 @@ class FormPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView( // Added this widget to enable scrolling
+        child: SingleChildScrollView(
           child: FormBuilder(
             key: _formKey,
             child: Column(
@@ -25,19 +30,19 @@ class FormPage extends StatelessWidget {
               children: [
                 _formController.responseCustomWidget(
                   'form',
-                  'firstname',
+                  'firstName',
                   'First Name',
                 ),
                 SizedBox(height: 16),
                 _formController.responseCustomWidget(
                   'form',
-                  'lastname',
+                  'lastName',
                   'Last Name',
                 ),
                 SizedBox(height: 16),
                 _formController.responseCustomWidget(
                   'phoneNumber',
-                  'phone',
+                  'phoneNumber',
                   'Phone Number',
                 ),
                 SizedBox(height: 16),
@@ -56,8 +61,7 @@ class FormPage extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState?.saveAndValidate() ?? false) {
-                      final formData = _formKey.currentState?.value;
-                      Get.snackbar('Form Submitted', formData.toString());
+                      _submitForm(context);
                     } else {
                       Get.snackbar('Error', 'Please fill all required fields');
                     }
@@ -70,5 +74,42 @@ class FormPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _submitForm(BuildContext context) async {
+    final formData = _formKey.currentState?.value;
+
+    print("Form Data: $formData");
+
+    final requestJson = {
+      "firstName": formData!['firstName'].toString().trim(),
+      "lastName": formData['lastName'].toString().trim(),
+      "phoneNumber": formData['phoneNumber'].toString().trim().substring(4),
+      "email": formData['email'].toString().trim(),
+      "address": formData['address'].toString().trim()
+    };
+
+    try {
+      // Replace with your actual API URL
+      final url =
+          Uri.parse('${Constants.SERVER_BASE_URL_API}/customers/create');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(requestJson),
+      );
+
+      if (response.statusCode == 200) {
+        Navigator.pop(context);
+        // Get.snackbar('Success', 'Customer data submitted successfully');
+        print("Success: Customer data submitted successfully");
+      } else {
+        print('Error: ${response.statusCode}: ${response.body}');
+        // Get.snackbar('Error', 'Error: ${response.statusCode}: ${response.body}');
+      }
+    } catch (e) {
+      print('Error: $e');
+      // Get.snackbar('Error', 'Error: $e');
+    }
   }
 }
