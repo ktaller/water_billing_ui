311a4a8aaf41a3dfea80f50694932a73bd65a854
George Ngigi <ngigegeorge023@gmail.com>
2025-01-27 17:00:37 +0300

    Save meter reading to the server.

--- lib/screens/meter_reading_form_page.dart
+++ lib/screens/meter_reading_form_page.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:water_billing_ui/constants/constants.dart';
 
 class MeterReadingFormPage extends StatefulWidget {
-  const MeterReadingFormPage({super.key});
+  final String meterNumber; // Receive meter number as argument
+
+  const MeterReadingFormPage({required this.meterNumber, super.key});
 
   @override
   State<MeterReadingFormPage> createState() => _MeterReadingFormPageState();
@@ -10,10 +16,11 @@ class MeterReadingFormPage extends StatefulWidget {
 class _MeterReadingFormPageState extends State<MeterReadingFormPage> {
   final TextEditingController _readingController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
 
-  void _submitReading() {
+  void _submitReading() async {
     if (_formKey.currentState!.validate()) {
-      final reading = int.parse(_readingController.text);
+      final reading = double.parse(_readingController.text);
 
       showDialog(
         context: context,
@@ -22,16 +29,56 @@ class _MeterReadingFormPageState extends State<MeterReadingFormPage> {
           content: Text('Submit the reading: $reading m³?'),
           actions: [
             TextButton(
-              onPressed: () => Navigator.of(context).pop(), // Close the dialog
+              onPressed: () => Navigator.of(context).pop(),
               child: const Text('Cancel'),
             ),
             ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Reading submitted successfully!')),
-                );
-                _readingController.clear(); // Clear the form
+              onPressed: () async {
+                Navigator.of(context).pop();
+
+                setState(() {
+                  _isLoading = true;
+                });
+
+                try {
+                  final url = Uri.parse(
+                      '${Constants.SERVER_BASE_URL_API}/meter-readings/create');
+                  final meterReadingData = {
+                    "meterNumber": widget.meterNumber,
+                    "currentReading": reading,
+                  };
+
+                  final response = await http.post(
+                    url,
+                    headers: {'Content-Type': 'application/json'},
+                    body: jsonEncode(meterReadingData),
+                  );
+
+                  if (response.statusCode == 200) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text('Reading submitted successfully!')),
+                    );
+                    _readingController.clear();
+                    Navigator.pop(context);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                          content: Text(
+                              'Error: ${response.statusCode} - ${response.body}')),
+                    );
+                    print('Error: ${response.statusCode}: ${response.body}');
+                  }
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Error: $e')),
+                  );
+                  print('Error: $e');
+                } finally {
+                  setState(() {
+                    _isLoading = false;
+                  });
+                }
               },
               child: const Text('Submit'),
             ),
@@ -42,7 +89,6 @@ class _MeterReadingFormPageState extends State<MeterReadingFormPage> {
   }
 
   void _navigateBackToCustomerList() {
-    // Navigate back multiple pages
     Navigator.of(context).popUntil((route) => route.isFirst);
   }
 
@@ -52,56 +98,59 @@ class _MeterReadingFormPageState extends State<MeterReadingFormPage> {
       appBar: AppBar(
         title: const Text('Meter Reading Form'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  controller: _readingController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Meter Reading',
-                    border: OutlineInputBorder(),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator()) // Show loading indicator
+          : Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        controller: _readingController,
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        decoration: const InputDecoration(
+                          labelText: 'Meter Reading',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a reading.';
+                          }
+                          try {
+                            double.parse(value); // Try parsing to double
+                          } catch (e) {
+                            return 'Enter a valid number.';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 30),
+                      SizedBox(
+                        width: 200.0,
+                        child: ElevatedButton(
+                          onPressed: _submitReading,
+                          child: const Text(
+                            'Submit',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 22),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: _navigateBackToCustomerList,
+                        child: const Text('Back to List'),
+                      ),
+                    ],
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a reading.';
-                    }
-                    final intValue = int.tryParse(value);
-                    if (intValue == null || intValue <= 0) {
-                      return 'Enter a valid positive number.';
-                    }
-                    return null;
-                  },
                 ),
-                const SizedBox(height: 30),
-                SizedBox(
-                  width: 200.0, // Fixed width
-                  child: ElevatedButton(
-                    onPressed: _submitReading,
-                    child: const Text(
-                      'Submit',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22), // Make the text bold
-                    ),
-                  ),
-                ),
-
-
-
-                const SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: _navigateBackToCustomerList,
-                  child: const Text('Back to List'),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
